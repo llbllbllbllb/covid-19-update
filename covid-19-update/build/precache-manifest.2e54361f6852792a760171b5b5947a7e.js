c25b6a82f396d8919d857289460fb1096b89ba2d
llbllbllbllb <295319725@qq.com>
2020-08-28 11:15:32 -0400

    build commit

---
+++ covid-19-update/build/precache-manifest.2e54361f6852792a760171b5b5947a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61cf040061d6216bbc6e685838db142",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1ac176f616e3db2dface",
+    "url": "./static/css/main.4987bd87.chunk.css"
+  },
+  {
+    "revision": "a67e94253ddbcb23a35e",
+    "url": "./static/js/2.bdf598b5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "./static/js/2.bdf598b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ac176f616e3db2dface",
+    "url": "./static/js/main.7d39fd1c.chunk.js"
+  },
+  {
+    "revision": "76e13918de2ae28f3b3f",
+    "url": "./static/js/runtime-main.55c230fd.js"
+  }
+]);
